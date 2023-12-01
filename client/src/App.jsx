c5cdc3f7bb6ba1835b534a99e80a1d0a784b5994
nader2356 <naderabdessaied8@gmail.com>
2023-12-01 19:39:19 +0100

    fix app.jsx file for signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,19 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css'
-import About from './pages/About';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
 import Search from './pages/Search';
-
-
-function App() {
-
-
+export default function App() {
   return (
     <BrowserRouter>
-     <Header />
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
@@ -27,7 +21,6 @@ function App() {
         <Route path='/about' element={<About />} />
         <Route path='/search' element={<Search />} />
         <Route path='/listing/:listingId' element={<Listing />} />
-
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
           <Route path='/create-listing' element={<CreateListing />} />
@@ -40,5 +33,3 @@ function App() {
     </BrowserRouter>
   );
 }
-
-export default App
